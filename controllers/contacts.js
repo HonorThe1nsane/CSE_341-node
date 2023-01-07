1299a460a32d94bd3e29cbaef34ea7cde93ee9f8
HonorThe1nsane <troymd2009@hotmail.com>
2023-01-06 17:13:23 -0700

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,6 @@
 const showSomeone = (req, res) => {
     res.send("Harmony Davidson <3!")
 }
-const showSomeone1 = (req, res) => {
-    res.send("Harmony <3 Davidson <3!")
-}
 
 
-module.exports = { showSomeone, showSomeone1 }
+module.exports = { showSomeone }

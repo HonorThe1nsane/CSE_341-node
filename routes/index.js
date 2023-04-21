852f56278caf72f3574845d077dcac961558b75d
HonorThe1nsane <troymd2009@hotmail.com>
2023-04-20 23:43:26 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const routes = require('express').Router();
 const { showSomeone } = require("../controllers/contacts")
 const { showSomeone1 } = require("../controllers/contacts")
 routes.get("/", showSomeone)
-// routes.get("/", showSomeone1)
+routes.get("/", showSomeone1)
 module.exports = routes;

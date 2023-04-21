9d9f9f8daa611f1b6b0c9da81c687c94e24a68b5
HonorThe1nsane <troymd2009@hotmail.com>
2023-04-20 23:57:08 -0600

    update and added REST CLIENT

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 
 
 const { showSomeone } = require("../controllers/contacts")
-const { showSomeone1 } = require("../controllers/contacts")
+
 routes.get("/", showSomeone)
-routes.get("/", showSomeone1)
+
 module.exports = routes;
